49767b6b276be74212755cb26ea0005901d8be6c
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-09-23 21:18:41 -0300

    Add the UI and the geolocator resources

---
+++ weatherapp/Common/Common.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Sensors;
+
+namespace weatherapp.Common
+{
+    public class Common
+    {
+        public static string API_LINK = "https://api.openweathermap.org/data/2.5/weather";
+        public static string API_KEY = "2e2d289822ca03230b80ad9c3ba868d6";
+
+        public static string APIRequest(string lat, string lon)
+        {
+            StringBuilder strBuilder = new StringBuilder(API_LINK);
+            //&units=metric to convert temp to celsius
+            strBuilder.AppendFormat("?lat={0}&lon={1}&appid={2}&units=metric", lat, lon, API_KEY);
+            return strBuilder.ToString();
+        }
+
+        public static DateTime ConvertUnixTimeToDateTime(double unix)
+        {
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dt = dt.AddSeconds(unix).ToLocalTime();
+            return dt;
+        }
+    }
+}
--- weatherapp/MainPage.xaml.cs
+++ weatherapp/MainPage.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +27,47 @@ namespace weatherapp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        string lat, lon;
         public MainPage()
         {
             this.InitializeComponent();
+            HideStatusBar();
+        }
+
+        private async void HideStatusBar()
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Phone.PhoneContract",1,0))
+            {
+                var statusBar = StatusBar.GetForCurrentView();
+                await statusBar.HideAsync();
+            }
+        }
+
+        private async void btnGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            progressRing.IsActive = true;
+            var geoLocator = new Geolocator();
+            geoLocator.DesiredAccuracy = PositionAccuracy.High;
+            Geoposition pos = await geoLocator.GetGeopositionAsync();
+
+            lat = pos.Coordinate.Point.Position.Latitude.ToString();
+            lon = pos.Coordinate.Point.Position.Longitude.ToString();
+
+            var data = await Helper.Helper.GetWeather(lat, lon);
+            if(data != null)
+            {
+                txtCity.Text = $"{data.name},{data.sys.country}";
+                txtLastUpdate.Text = $"Last Updated: {DateTime.Now.ToString("dd MMMM yyyy HH:mm")}";
+
+                BitmapImage image = new BitmapImage(new Uri($"http://openweathermap.org/img/w/{data.weather[0].icon}.png",UriKind.Absolute));
+                imgWeather.Source = image;
+
+                txtDescription.Text = $"{data.weather[0].description}";
+                txtHumidity.Text = $"Humidity: {data.main.humidity}%";
+                txtTime.Text = $"{Common.Common.ConvertUnixTimeToDateTime(data.sys.sunrise).ToString("HH:mm")}/ {Common.Common.ConvertUnixTimeToDateTime(data.sys.sunset).ToString("HH:mm")}";
+                txtCel.Text = $"{data.main.temp} °C";
+            }
+            progressRing.IsActive = false;
         }
     }
 }
